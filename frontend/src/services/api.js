664c023b40c753883d19f87fea6d2c37cf59527c
Akash Chaudhary <akashchaudhary5100@gmail.com.com>
2026-06-15 09:24:07 +0530

    Update API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    "https://inventory-order-management-system-production-0b40.up.railway.app",
 });
 
 export default api;
