6b25c474af7fa7503f3e36cecc38e06922a262b3
Akash Chaudhary <akashchaudhary5100@gmail.com.com>
2026-06-15 13:32:01 +0530

    Update Railway backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "https://inventory-order-management-system-production-0b40.up.railway.app",
+    "https://inventory-order-management-system-production-5bf4.up.railway.app",
 });
 
 export default api;
