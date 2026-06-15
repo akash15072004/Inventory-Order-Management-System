5e5a48526f5a55a2880521832685815fad3bb7dd
Akash Chaudhary <akashchaudhary5100@gmail.com.com>
2026-06-15 21:59:23 +0530

    Add Vercel URL to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,15 +28,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://localhost:5175",
         "http://localhost:5176",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-        "http://127.0.0.1:5175",
-        "http://127.0.0.1:5176",
-        "https://inventory-order-management-system-gold.vercel.app",
-        "https://inventory-order-management-system-akash-chaudhary.vercel.app",
-        "https://inventory-order-management-system-git-main-akash-chaudhary.vercel.app",
-        "https://inventory-order-management-system-km893h8an-akash-chaudhary.vercel.app",
-        
+        "https://inventory-order-management-system-gold.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
