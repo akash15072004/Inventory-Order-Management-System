085da48363b841ccb2306d19f5588e3367f3fdee
Akash Chaudhary <akashchaudhary5100@gmail.com.com>
2026-06-15 21:51:41 +0530

    Use Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://127.0.0.1:8000"
-      : "https://inventory-order-management-system-production123.up.railway.app",
+      : "https://inventory-order-management-system1-41dk.onrender.com",
 });
 
 export default api;
